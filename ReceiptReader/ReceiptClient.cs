08206c49d9a5b6fdbf52e49723902d756b28acb5
glabular <glu.max06@gmail.com>
2026-02-19 22:53:16 +0100

    Added ReceiptClient class

---
+++ ReceiptReader/ReceiptClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReceiptReader;
+
+internal sealed class ReceiptClient : IDisposable
+{
+    public void Dispose() => throw new NotImplementedException();
+}
