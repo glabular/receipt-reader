7761bb465e442b8335c13b5551261399b4f6876f
glabular <glu.max06@gmail.com>
2026-02-19 15:28:01 +0100

    Init commit

---
+++ ReceiptReader/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ReceiptReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
