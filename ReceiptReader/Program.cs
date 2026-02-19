d44dd256f79bd5ac0cca1d3d33037c4de0fbe71c
glabular <glu.max06@gmail.com>
2026-02-19 16:09:42 +0100

    Implemented title extraction for test

--- ReceiptReader/Program.cs
+++ ReceiptReader/Program.cs
@@ -6,6 +6,28 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+			try
+			{
+                Console.WriteLine("Enter a URL to fetch the title:");
+                var url = Console.ReadLine()?.Trim();
+                using var client = new HttpClient();
+                var html = await client.GetStringAsync(url);
+                var doc = new HtmlDocument();
+
+                doc.LoadHtml(html);
+
+                var title = doc.DocumentNode.SelectSingleNode("//title")?.InnerText;
+
+                Console.WriteLine(title);
+                Console.WriteLine("---");
+            }
+			catch (Exception ex)
+			{
+                Console.WriteLine("Something went wrong:");
+                Console.WriteLine(ex.Message);
+			}
+        }        
     }
 }
