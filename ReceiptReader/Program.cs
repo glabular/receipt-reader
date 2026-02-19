fd16a0b2f1b5e8163dafd17a2138c8f9284af72c
glabular <glu.max06@gmail.com>
2026-02-19 19:16:53 +0100

    Added invoice parser service

--- ReceiptReader/Program.cs
+++ ReceiptReader/Program.cs
@@ -1,5 +1,4 @@
-﻿using HtmlAgilityPack;
-using ReceiptReader.Models;
+﻿using ReceiptReader.Models;
 using ReceiptReader.Services;
 using System;
 
@@ -18,51 +17,11 @@ internal class Program
         while (!IsUrlValid(url = Console.ReadLine()?.Trim()))
         {
             Console.Error.WriteLine("Invalid URL provided. Please try again:\n");
-        }        
+        }
 
         var pageSource = BrowserEngine.GetPageSource(url!); 
-        var htmlDoc = new HtmlDocument();
 
-        htmlDoc.LoadHtml(pageSource);
-
-        // Locate the main invoice items list
-        var invoiceList = htmlDoc.DocumentNode.SelectSingleNode(
-            "//ul[contains(@class,'invoice-items-list') and contains(@class,'list-unstyled')]"
-        );
-
-        var invoiceItems = new List<InvoiceItem>();
-
-        if (invoiceList != null)
-        {
-            var items = invoiceList.SelectNodes("./li[contains(@class,'invoice-item')]");
-            if (items == null)
-            {
-                Console.Error.WriteLine("No invoice items found.");
-                return;
-            }
-
-            foreach (var item in items)
-            {
-                var heading = item.SelectSingleNode(".//li[contains(@class,'invoice-item--heading')]");
-                var details = item.SelectSingleNode(".//li[contains(@class,'invoice-item--details')]");
-
-                if (heading == null)
-                {
-                    continue;
-                }
-
-                var invoiceItem = new InvoiceItem
-                {
-                    Title = heading.SelectSingleNode(".//span[contains(@class,'invoice-item--title')]")?.InnerText.Trim() ?? string.Empty,
-                    UnitPrice = heading.SelectSingleNode(".//span[contains(@class,'invoice-item--unit-price')]")?.InnerText.Trim() ?? string.Empty,
-                    InvoiceItemPrice = heading.SelectSingleNode(".//span[contains(@class,'invoice-item--price')]")?.InnerText.Trim() ?? string.Empty,
-                    Quantity = details.SelectSingleNode(".//span[contains(@class,'invoice-item--quantity')]")?.InnerText.Trim() ?? string.Empty
-                };
-
-                invoiceItems.Add(invoiceItem);                
-            }
-            
-        }
+        var invoiceItems = InvoiceParser.ParseInvoicePage(pageSource);
 
         foreach (var item in invoiceItems)
         {
---
+++ ReceiptReader/Services/InvoiceParser.cs
@@ -0,0 +1,54 @@
+﻿using HtmlAgilityPack;
+using ReceiptReader.Models;
+
+namespace ReceiptReader.Services;
+
+internal sealed class InvoiceParser
+{
+    public static List<InvoiceItem> ParseInvoicePage(string pageSource)
+    {
+        var htmlDoc = new HtmlDocument();
+
+        htmlDoc.LoadHtml(pageSource);
+
+        var invoiceList = htmlDoc.DocumentNode.SelectSingleNode(
+            "//ul[contains(@class,'invoice-items-list') and contains(@class,'list-unstyled')]"
+        );
+
+        var invoiceItems = new List<InvoiceItem>();
+
+        if (invoiceList != null)
+        {
+            var items = invoiceList.SelectNodes("./li[contains(@class,'invoice-item')]");
+            if (items == null)
+            {
+                Console.Error.WriteLine("No invoice items found.");
+                return invoiceItems;
+            }
+
+            foreach (var item in items)
+            {
+                var heading = item.SelectSingleNode(".//li[contains(@class,'invoice-item--heading')]");
+                var details = item.SelectSingleNode(".//li[contains(@class,'invoice-item--details')]");
+
+                if (heading == null)
+                {
+                    continue;
+                }
+
+                var invoiceItem = new InvoiceItem
+                {
+                    Title = heading.SelectSingleNode(".//span[contains(@class,'invoice-item--title')]")?.InnerText.Trim() ?? string.Empty,
+                    UnitPrice = heading.SelectSingleNode(".//span[contains(@class,'invoice-item--unit-price')]")?.InnerText.Trim() ?? string.Empty,
+                    InvoiceItemPrice = heading.SelectSingleNode(".//span[contains(@class,'invoice-item--price')]")?.InnerText.Trim() ?? string.Empty,
+                    Quantity = details.SelectSingleNode(".//span[contains(@class,'invoice-item--quantity')]")?.InnerText.Trim() ?? string.Empty
+                };
+
+                invoiceItems.Add(invoiceItem);
+            }
+
+        }
+
+        return invoiceItems;
+    }
+}
