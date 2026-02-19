110ba864bcaf5e5a25edb6e56d83750b49e6ba0f
glabular <glu.max06@gmail.com>
2026-02-19 19:19:16 +0100

    Added URL validator service

--- ReceiptReader/Program.cs
+++ ReceiptReader/Program.cs
@@ -1,20 +1,15 @@
-﻿using ReceiptReader.Models;
-using ReceiptReader.Services;
+﻿using ReceiptReader.Services;
 using System;
 
 namespace ReceiptReader;
 
 internal class Program
 {
-    private const string BaseUrl = "https://mapr.tax.gov.me/";
-    private const string VerifyEndpoint = "ic/#/verify?iic=";
-    private static readonly string VerificationUrlTemplate = $"{BaseUrl}{VerifyEndpoint}";
-
     static async Task Main(string[] args)
     {
         Console.WriteLine("Please enter receipt URL:");
         string? url;
-        while (!IsUrlValid(url = Console.ReadLine()?.Trim()))
+        while (!UrlValidatior.IsUrlValid(url = Console.ReadLine()?.Trim()))
         {
             Console.Error.WriteLine("Invalid URL provided. Please try again:\n");
         }
@@ -27,12 +22,5 @@ internal class Program
         {
             Console.WriteLine($"Title: {item.Title}, Unit Price: {item.UnitPrice}, Total Price: {item.InvoiceItemPrice}, Quantity: {item.Quantity}");
         }
-    }
-
-    private static bool IsUrlValid(string? url)
-    {
-        return !string.IsNullOrEmpty(url) &&
-               Uri.IsWellFormedUriString(url, UriKind.Absolute) &&
-               url.StartsWith(VerificationUrlTemplate);
     }
 }
---
+++ ReceiptReader/Services/UrlValidatior.cs
@@ -0,0 +1,15 @@
+﻿namespace ReceiptReader.Services;
+
+internal sealed class UrlValidatior
+{
+    private const string BaseUrl = "https://mapr.tax.gov.me/";
+    private const string VerifyEndpoint = "ic/#/verify?iic=";
+    private static readonly string VerificationUrlTemplate = $"{BaseUrl}{VerifyEndpoint}";
+
+    public static bool IsUrlValid(string? url)
+    {
+        return !string.IsNullOrEmpty(url) &&
+               Uri.IsWellFormedUriString(url, UriKind.Absolute) &&
+               url.StartsWith(VerificationUrlTemplate);
+    }
+}
