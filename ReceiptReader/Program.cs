961b8059dbfec0d01b6155444fe355bb703637ee
glabular <glu.max06@gmail.com>
2026-02-24 13:03:41 +0100

    Code cleanup

--- ReceiptReader/Program.cs
+++ ReceiptReader/Program.cs
@@ -1,11 +1,10 @@
-﻿using ReceiptReader.Services;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace ReceiptReader;
 
 internal class Program
 {
-    static async Task Main(string[] args)
+    static async Task Main()
     {
         IConfiguration config = new ConfigurationBuilder()
             .AddUserSecrets<Program>()
@@ -25,35 +24,5 @@ internal class Program
         await telegramClient.StartAsync();
 
         Console.ReadLine();
-
-        using var client = new ReceiptClient();
-
-        while (true)
-        {
-            Console.WriteLine("Please enter receipt URL:");
-            var url = Console.ReadLine()?.Trim();
-            if (!UrlValidator.IsUrlValid(url))
-            {
-                Console.Error.WriteLine("Invalid URL provided. Please try again.\n");
-                continue;
-            }
-
-            var invoiceResult = client.GetInvoice(url!);
-
-            Console.WriteLine($"You spent {invoiceResult.TotalSum} in '{invoiceResult.ShopName}' at {invoiceResult.ShoppingDate}");
-
-            if (invoiceResult?.BoughtItems == null || invoiceResult.BoughtItems.Count == 0)
-            {
-                Console.WriteLine("No items found in the receipt.");
-                continue;
-            }
-
-            foreach (var item in invoiceResult.BoughtItems)
-            {
-                Console.WriteLine($"Title: {item.Name}, Unit Price: {item.UnitPrice}, Total Price: {item.TotalPrice}, Quantity: {item.Quantity}");
-            }
-
-            Console.WriteLine("-----\n\n");
-        }
     }
 }
