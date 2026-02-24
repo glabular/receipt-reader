977bbd17e20b24185f1f6da0cc31b6cab8bb9f61
glabular <glu.max06@gmail.com>
2026-02-24 14:57:56 +0100

    Added WeChat qr reader

---
+++ ReceiptReader/Services/WeChatQrReader.cs
@@ -0,0 +1,38 @@
+﻿using OpenCvSharp;
+
+namespace ReceiptReader.Services;
+
+internal class WeChatQrReader : IDisposable
+{
+    private readonly WeChatQRCode _detector;
+
+    public WeChatQrReader()
+    {
+        var modelPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Models", "OpenCvSharp4");
+
+        _detector = WeChatQRCode.Create(
+            $"{modelPath}/detect.prototxt",
+            $"{modelPath}/detect.caffemodel",
+            $"{modelPath}/sr.prototxt",
+            $"{modelPath}/sr.caffemodel"
+        );
+    }
+
+    public string? ReadQr(string imagePath)
+    {
+        using var mat = Cv2.ImRead(imagePath);
+        if (mat.Empty())
+        {
+            throw new Exception("Could not load image.");
+        }
+
+        _detector.DetectAndDecode(mat, out Mat[] points, out string[] results);
+
+        return results.Length > 0 ? results[0] : null;
+    }
+
+    public void Dispose()
+    {
+        _detector?.Dispose();
+    }
+}
