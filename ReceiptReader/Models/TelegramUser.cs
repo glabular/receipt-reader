f891576c7570045f6afe94c51a7cd8b271d224cc
glabular <glu.max06@gmail.com>
2026-02-25 22:28:49 +0100

    Added TelegramUser class

---
+++ ReceiptReader/Models/TelegramUser.cs
@@ -0,0 +1,10 @@
+﻿namespace ReceiptReader.Models;
+
+internal sealed class TelegramUser
+{
+    public int Id { get; set; }
+
+    public long TelegramUserId { get; set; }
+
+    public List<Invoice> Invoices { get; set; } = [];
+}
