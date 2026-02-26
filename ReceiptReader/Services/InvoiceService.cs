452f946e4dcd519f249a3582cc7369ef1bcdea37
glabular <glu.max06@gmail.com>
2026-02-26 20:57:44 +0100

    Added InvoiceService

---
+++ ReceiptReader/Services/InvoiceService.cs
@@ -0,0 +1,20 @@
+﻿using ReceiptReader.Data;
+using ReceiptReader.Models;
+
+namespace ReceiptReader.Services;
+
+internal class InvoiceService
+{
+    private readonly BotDbContext _dbContext;
+
+    public InvoiceService(BotDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task AddInvoiceAsync(Invoice invoice)
+    {
+        await _dbContext.Invoices.AddAsync(invoice);
+        await _dbContext.SaveChangesAsync();
+    }
+}
