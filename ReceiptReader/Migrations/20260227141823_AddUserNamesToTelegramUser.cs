500d9caef447fdab9b2dacfe7b8555e337e96fb5
glabular <glu.max06@gmail.com>
2026-02-27 15:19:51 +0100

    Applied AddUserNamesToTelegramUser migration

---
+++ ReceiptReader/Migrations/20260227141823_AddUserNamesToTelegramUser.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReceiptReader.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserNamesToTelegramUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "TelegramUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "TelegramUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "TelegramUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "TelegramUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "TelegramUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "TelegramUsers");
+        }
+    }
+}
