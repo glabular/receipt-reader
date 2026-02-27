7060e7f13585e3d9713dbf57fce86aa1a2dfe61a
glabular <glu.max06@gmail.com>
2026-02-27 14:08:53 +0100

    Extended telegram user class

--- ReceiptReader/Models/TelegramUser.cs
+++ ReceiptReader/Models/TelegramUser.cs
@@ -7,4 +7,10 @@ internal sealed class TelegramUser
     public long TelegramUserId { get; set; }
 
     public List<Invoice> Invoices { get; set; } = [];
+
+    public string? Username { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
 }
