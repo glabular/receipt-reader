d00b35a2af9de79593aff0df5a43d05cf42914ac
glabular <glu.max06@gmail.com>
2026-03-01 12:56:13 +0100

    Added migrations

---
+++ ReceiptReader/Migrations/20260301115141_ChangeForeignKey.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReceiptReader.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_TelegramUsers_TelegramUserId",
+                table: "Invoices");
+
+            migrationBuilder.RenameColumn(
+                name: "TelegramUserId",
+                table: "Invoices",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Invoices_TelegramUserId",
+                table: "Invoices",
+                newName: "IX_Invoices_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_TelegramUsers_UserId",
+                table: "Invoices",
+                column: "UserId",
+                principalTable: "TelegramUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_TelegramUsers_UserId",
+                table: "Invoices");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Invoices",
+                newName: "TelegramUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Invoices_UserId",
+                table: "Invoices",
+                newName: "IX_Invoices_TelegramUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_TelegramUsers_TelegramUserId",
+                table: "Invoices",
+                column: "TelegramUserId",
+                principalTable: "TelegramUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
