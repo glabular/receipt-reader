b44acc9af674236806d63cf253e4dc4fd66dc736
glabular <glu.max06@gmail.com>
2026-04-02 01:35:09 +0200

    Added LogsDirectoryResolver for default log folder path

---
+++ ReceiptReader/Services/LogsDirectoryResolver.cs
@@ -0,0 +1,18 @@
+﻿namespace ReceiptReader.Services;
+
+internal static class LogsDirectoryResolver
+{
+    public static string ResolveFromOptionalPath(string? configuredPath)
+    {
+        var trimmedPath = configuredPath?.Trim();
+
+        if (string.IsNullOrWhiteSpace(trimmedPath))
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ReceiptReader", "Logs");
+        }
+        else
+        {
+            return trimmedPath;
+        }
+    }
+}
